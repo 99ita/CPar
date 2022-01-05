5167d75978fe2961ce6743d54ff007d0f5cb3070
ita <eduardojafaraujo@gmail.com>
2022-01-05 07:19:42 +0000

    insertion sort para listas ligadas

--- seq.c
+++ seq.c
@@ -25,13 +25,17 @@ Plano:
 
         -Listas ligadas: Resolve o problema dos arrays dinamicos e o overhead de memória não e muito grande
                          nesta estrategia ao ordenar os buckets podia-se logo ordenar para um array usando 
-                         insertion sort
+                         insertion sort (é preciso saber o tamanho total do bucket:
+                                            -criar uma "superstruct" que guarda sempre o tamanho da ll
+                                            -percorrer a ll para saber o tamanho do bucket)
+
+        Ja implementei insertionsort para arrays e para listas ligadas a ordenar logo para um array
 
     -Percorrer o array para preencher os buckets*
 
     -Percorrer os buckets ordenando-os*
 
-    -Qual a melhor maneira de juntar os arrays ordenados correspondentes a cada bucket?
+    -Qual a melhor maneira de juntar os arrays ordenados correspondentes a cada bucket?*
         -Pode se tentar criar logo o array total e depois percorrer os buckets sequencialmente (dificil de paralelizar)
         -Dividir o sorting dos buckets por threads e atribuir uma tag a cada pacote de buckets (que será assigned a uma thread)
         de forma a mais tarde conseguirmos juntar todos
@@ -42,54 +46,50 @@ Plano:
 
 
 
-
-
-
-
 // Linked list structure to represent a bucket
 struct Node{
     int data;
     struct Node *next;
 };
  
-// Add node to head of linked list 
+// Add node to head of linked list (net)
 void push(struct Node** head, int value){
-    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+    struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
  
-    new_node->data  = value;
+    newNode->data  = value;
  
-    new_node->next = (*head);
+    newNode->next = (*head);
  
-    (*head)    = new_node;
+    (*head)    = newNode;
 }
  
-// Add node after input node 
-void insertAfter(struct Node* prev_node, int new_data){
-    if (prev_node == NULL){
+// Add node after input node (net)
+void insertAfter(struct Node* prevNode, int value){
+    if (prevNode == NULL){
       printf("ERROR: The given previous node cannot be NULL!");
       return;
     }
  
-    struct Node* new_node =(struct Node*) malloc(sizeof(struct Node));
+    struct Node* newNode =(struct Node*) malloc(sizeof(struct Node));
  
-    new_node->data  = new_data;
+    newNode->data  = value;
  
-    new_node->next = prev_node->next;
+    newNode->next = prevNode->next;
  
-    prev_node->next = new_node;
+    prevNode->next = newNode;
 }
  
-// Add node to end of linked list 
-void append(struct Node** head_ref, int new_data){
-    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
-    struct Node *last = *head_ref;  
+// Add node to end of linked list (net)
+void append(struct Node** head, int value){
+    struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
+    struct Node *last = *head;  
  
-    new_node->data  = new_data;
+    newNode->data  = value;
  
-    new_node->next = NULL;
+    newNode->next = NULL;
  
-    if (*head_ref == NULL){
-       *head_ref = new_node;
+    if (*head == NULL){
+       *head = newNode;
        return;
     }
  
@@ -97,40 +97,47 @@ void append(struct Node** head_ref, int new_data){
         last = last->next;
  
 
-    last->next = new_node;
+    last->next = newNode;
     return;
 }
  
-// Utility function to print a linked list
-void printList(struct Node *node)
-{
-    while (node != NULL)
-    {
-       printf(" %d ", node->data);
-       node = node->next;
+// Utility function to print a linked list (net)
+void printList(struct Node *node){
+    while (node != NULL){
+        printf(" %d ", node->data);
+        node = node->next;
     }
     printf("\n\n");
 }
 
-/* Insertion sort algorithm complexity O(n^2) but in cases 
-where elements are close to their final position behaves like O(n)
-applied to*/
-void insertionSortLL(int* arr, int n){
-    int i, key, j;
+// Insertion sort algorithm applied to linked list, returns array (eu)
+int* insertionSortLL(struct Node** head, int size){
+    struct Node* node = *head;
+    int* arr = (int*) malloc(sizeof(int)*size);
 
-    // Assumes first element is ordered (i = 1)
-    for (i = 1; i < n; i++) {
-        key = arr[i];
-        j = i - 1;
- 
-        // Shifts elements greater than key one position ahead
-        while(j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
+    for(int i = 0; node != NULL; i++){
+        int key = node->data;
+        if(i == 0){ //if first element insert to first position of array
+            arr[0] = key;
+        }else{ //else insert orderly
+            int j = i-1;
+            while(j >= 0 && arr[j] > key){ //While the value is greater than key shift the value forward by one 
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key; ///When the value stops being greater than key we found key's position
         }
+        node = node->next;
+    }
+    return arr;
+}   
 
-        // Position of current key found
-        arr[j + 1] = key;
+//Utility function to generate random linked list of size n(eu)
+void randomLinkedList(struct Node** head, int n){
+    if(*head!=NULL) return; //Head must be NULL
+    srand(getpid());
+    for(;n > 0; n--){
+        push(head,rand());
     }
 }
 
@@ -157,9 +164,7 @@ void insertionSortLL(int* arr, int n){
 
 
 
-
-
-void bucketSort(int* array, int n){  
+void bucketSort(int* array, int n){ //(net, nao funciona se o valor mais alto for maior que o numero de valores)
     int i, j;  
     int count[n]; 
     for (i = 0; i < n; i++)
@@ -173,12 +178,20 @@ void bucketSort(int* array, int n){
             array[j++] = i;
 }   
 
-
+void verifyOrdered(int* arr, int size){
+    for(int i = 1; i < size; i++){
+        if(arr[i-1] > arr[i]){
+            printf("Array isn't ordered!\nArray[%d] = %d and Array[%d] = %d",i-1,i,arr[i-1],arr[i]);
+            return;
+        }
+    }
+    printf("Array is ordered!\n");
+}
 
 
 /* Insertion sort algorithm complexity O(n^2) but in cases 
-where elements are close to their final position behaves like O(n)*/
-void insertionSort(int* arr, int n){
+where elements are close to their final position behaves like O(n)*/ //(net)
+void insertionSort(int* arr, int n){ 
     int i, key, j;
 
     // Assumes first element is ordered (i = 1)
@@ -197,21 +210,24 @@ void insertionSort(int* arr, int n){
     }
 }
  
-//Utility function to print the array
+//Utility function to print the array (net)
 void printArr(int* arr, int n){
     int i;
     for (i = 0; i < n; i++) printf("%d ", arr[i]);
     printf("\n");
 }
 
-//Utility function to generate random array of size n
-int* randArr(int n){
-   int* randArr = (int*) malloc(sizeof(int)*n);
-   for(int i = 0; i < n; i++) randArr[i]=rand();   //Generate number between 0 to 99
-   return randArr;
+//Utility function to generate random array of size n (eu)
+int* randomArr(int n){
+    srand(getpid());
+    int* randArr = (int*) malloc(sizeof(int)*n);
+    for(int i = 0; i < n; i++) randArr[i]=rand();   //Generate number between 0 to 99
+    return randArr;
 }
 
-//Function to validate argument
+
+
+//Function to validate argument (eu)
 int valid(char* s){
     return atoi(s);
 }
@@ -221,32 +237,6 @@ int valid(char* s){
 int main(int argc, char* argv[]){
     int size;
 
-
-    struct Node* head = NULL;
- 
-    append(&head, 6);
- 
-    push(&head, 7);
- 
-    push(&head, 1);
- 
-    append(&head, 4);
- 
-  
-    insertAfter(head->next, 8);
- 
-    printf("\n Created Linked list is: ");
-    printList(head);
- 
-    return 0;
-
-
-
-
-
-
-
-/*
     //----------Input control----------
     if(argc < 2){
         printf("\nERROR: Not enough arguments!\n./seq <Size of Array>\n\n");
@@ -263,24 +253,33 @@ int main(int argc, char* argv[]){
     //-------------------------------------
 
 
-    printf("\nRAND_MAX: %d\nSize of Array: %d\n\n",RAND_MAX,size);
 
-    int* arr = randArr(size);
+    printf("\nRAND_MAX: %d\nSize of Array: %d\n",RAND_MAX,size);
+
+    int* arr = randomArr(size);
  
     clock_t begin = clock();
     insertionSort(arr, size);
+    verifyOrdered(arr, size);
     clock_t end = clock();
 
     double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 
-    printf("\nTime elapsed: %fs\n\n",time_spent);
+    printf("\nArray to Array\nTime elapsed: %fs\n",time_spent);
+
+
+
+
+    struct Node* head = NULL;
+    randomLinkedList(&head,size);
 
     clock_t begin2 = clock();
-    bucketSort(arr, size);
+    verifyOrdered(insertionSortLL(&head, size),size);
     clock_t end2 = clock();
 
     double time_spent2 = (double)(end2 - begin2) / CLOCKS_PER_SEC;
 
-    printf("\nTime elapsed: %fs\n\n",time_spent2);*/
-
+    printf("\nLinked list to Array\nTime elapsed: %fs\n\n",time_spent2);
+    
+    return 0;
 }
